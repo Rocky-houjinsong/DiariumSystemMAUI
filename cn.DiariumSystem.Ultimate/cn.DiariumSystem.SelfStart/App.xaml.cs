7a14d35c89e0896d75dcef8ecbe1cf57cece67cf
宋厚锦 <songhoujin@mediinfo.com.cn>
2024-02-23 17:43:28 +0800

    初始化 项目 并添加注释

---
+++ cn.DiariumSystem.Ultimate/cn.DiariumSystem.SelfStart/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace cn.DiariumSystem.SelfStart
+{
+    /// <summary>
+    /// APP启动加载页
+    /// </summary>
+    /// <remarks>
+    /// <para>类型 基本为  {Shell}</para>
+    /// <para>通过 {MainPage}  设置</para>
+    /// </remarks>
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ cn.DiariumSystem.Ultimate/cn.DiariumSystem.SelfStart/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace cn.DiariumSystem.SelfStart
+{
+    /// <summary>
+    /// 作用待定 ??? 
+    /// </summary>
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
